a79815aea1a4ec376101880ae507c88c4257734e
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2022-01-05 18:25:53 -0600

    Transitioning over to have both a REST API and an SSR site

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask.json import jsonify
 import json
 
-
 film_file = open('data/films.json', "r")
 films = json.loads(film_file.read())
 film_file.close()
@@ -24,6 +23,7 @@ vehicles = json.loads(vehicles_file.read())
 vehicles_file.close()
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -36,6 +36,11 @@ def all_films():
     return render_template("films/all.html", films=films)
 
 
+@app.get('/api/films')
+def api_all_films():
+    return jsonify(films)
+
+
 @app.get('/films/<film_id>')
 def single_film(film_id):
     found_film = {}
@@ -46,75 +51,71 @@ def single_film(film_id):
         hours = int(int(film['running_time']) / 60)
         minutes = int(film['running_time']) % 60
         film_with_time = {**found_film, "hours": hours, "minutes": minutes}
-        print(film_with_time)
         return render_template("films/single.html", film=film_with_time)
     else:
         return render_template("not_found.html", resource_name="films")
 
 
-@app.get('/people')
+@app.get('/api/films/<film_id>')
+def api_single_film(film_id):
+    found_film = {}
+    for film in films:
+        if (film['id'] == film_id):
+            found_film = film
+    return found_film
+
+
+@app.get('/api/people')
 def all_people():
     return jsonify(people)
 
 
-@app.get('/people/<person_id>')
+@app.get('/api/people/<person_id>')
 def single_person(person_id):
     found_person = {}
     for person in people:
         if (person['id'] == person_id):
             found_person = person
-    if (found_person.__len__() > 0):
-        return found_person
-    else:
-        return {"message": "No person with that ID was located"}, 404
+    return found_person
 
 
-@app.get('/locations')
+@app.get('/api/locations')
 def all_locations():
     return jsonify(locations)
 
 
-@app.get('/locations/<location_id>')
+@app.get('/api/locations/<location_id>')
 def single_location(location_id):
     found_location = {}
     for location in locations:
         if (location['id'] == location_id):
             found_location = location
-    if (found_location.__len__() > 0):
-        return found_location
-    else:
-        return {"message": "No location with that ID was located"}, 404
+    return found_location
 
 
-@app.get('/species')
+@app.get('/api/species')
 def all_species():
     return jsonify(species)
 
 
-@app.get('/species/<species_id>')
+@app.get('/api/species/<species_id>')
 def single_species(species_id):
     found_species = {}
     for single in species:
         if (single['id'] == species_id):
             found_species = single
-    if (found_species.__len__() > 0):
-        return found_species
-    else:
-        return {"message": "No species with that ID was located"}, 404
+    return found_species
 
 
-@app.get('/vehicles')
+@app.get('/api/vehicles')
 def all_vehicles():
     return jsonify(vehicles)
 
 
-@app.get('/vehicles/<vehicle_id>')
+@app.get('/api/vehicles/<vehicle_id>')
 def single_vehicle(vehicle_id):
     found_vehicle = {}
     for vehicle in vehicles:
         if (vehicle['id'] == vehicle_id):
             found_vehicle = vehicle
-    if (found_vehicle.__len__() > 0):
-        return found_vehicle
-    else:
-        return {"message": "No vehicle with that ID was located"}, 404
+    return found_vehicle
